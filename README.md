aee88825aa7a5507046fd55096bcc74dfd48105e
SOULEYSANOGO <SOULEYSANOGO@CIEW15854>
2021-08-08 07:49:31 +0000

    update

--- README.md
+++ README.md
@@ -3,6 +3,12 @@
 - https://howtodoinjava.com/spring-boot2/pagination-sorting-example/
 - https://www.baeldung.com/spring-data-rest-customize-http-endpoints
 - https://spring.io/guides/tutorials/rest/
+- https://mkyong.com/spring-boot/spring-boot-spring-data-elasticsearch-example/
+
+- https://dbdiagram.io/d
+
+## API Doc
+- http://localhost:8080/api/
 
 ## DB  InMemory
 
--- src/main/java/com/macrosoft/apifirst/repository/ClientRepository.java
+++ src/main/java/com/macrosoft/apifirst/repository/ClientRepository.java
@@ -16,6 +16,34 @@ public interface ClientRepository extends JpaRepository<Client, Long> {
     @RestResource(exported = false)
     public void deleteById(Long aLong);
 	*/
+	
+	/*
+	Assume Persons have Addresses
+	List<Person> findByAddress_ZipCode(ZipCode zipCode);
+	*/
+	
+	
+	/*
+		CREATE VIEW Vue
+	(colonneA,colonneB,colonneC,colonneD)
+	AS SELECT colonne1,colonne2,colonneI,colonneII
+	FROM Nom_table1 Alias1,Nom_tableII AliasII
+	WHERE Alias1.colonne1 = AliasII.colonneI
+	AND Alias1.colonne2 = AliasII.colonneII
+	Les vues ainsi créées peuvent être l'objet de nouvelles requêtes en précisant le nom de la vue au lieu d'un nom de table dans un ordre SELECT...
+	*/
+	
+	
+	/*
+	Types of Caching
+	There are four types of caching are as follows:
+
+	In-memory Caching
+	Database Caching
+	Web server Caching
+	CDN Caching
+	In-memory Caching
+	*/
 
 }
 
